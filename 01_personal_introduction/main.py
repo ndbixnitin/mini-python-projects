0520f2b5fd0011a1de7017af97b03e700f6ed7ce
nitinbca79-ai <nitinbca79@gmail.com>
2026-08-18 13:32:51 +0530

    Add Day 1 personal introduction project

---
+++ 01_personal_introduction/main.py
@@ -0,0 +1,25 @@
+# ==========================================
+# Day 1 - Personal Introduction Generator
+# ==========================================
+
+# Personal Information
+name = "Nitin Dubey"
+age = 17
+city = "Gonda"
+course = "BCA"
+
+# Program Title
+print("======================================")
+print("      PERSONAL INTRODUCTION")
+print("======================================")
+
+# Display Personal Information
+print("Name   :", name)
+print("Age    :", age)
+print("City   :", city)
+print("Course :", course)
+
+# Final Message
+print("======================================")
+print("Thank you for reading my introduction!")
+print("======================================")
