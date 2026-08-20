59b8e51c0e39ea50cb15bfb64e314b1ac77142d6
nitinbca79-ai <nitinbca79@gmail.com>
2026-08-20 21:34:16 +0530

    Add Day 3 age calculator project

---
+++ 03_age_calculator/main.py
@@ -0,0 +1,24 @@
+# ==========================================
+# Day 3 - Age Calculator
+# ==========================================
+
+print("======================================")
+print("           AGE CALCULATOR")
+print("======================================")
+
+# Taking birth year from user
+birth_year = int(input("Enter your birth year: "))
+
+# Current year
+current_year = 2026
+
+# Calculate age
+age = current_year - birth_year
+
+# Display result
+print("\n----- Result -----")
+print("Your age is:", age, "years")
+
+print("======================================")
+print("       Calculation Completed!")
+print("======================================")
