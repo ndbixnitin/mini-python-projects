da9aa8e99f1f3315893b4d0ec463ad16fd613162
nitinbca79-ai <nitinbca79@gmail.com>
2026-08-21 20:26:36 +0530

    Add Day 4 even odd checker project

---
+++ 04_even_odd_checker/└── main.py
@@ -0,0 +1,20 @@
+# ==========================================
+# Day 4 - Even or Odd Number Checker
+# ==========================================
+
+print("======================================")
+print("       EVEN OR ODD CHECKER")
+print("======================================")
+
+# Taking number from user
+number = int(input("Enter a number: "))
+
+# Check whether number is even or odd
+if number % 2 == 0:
+    print("\nThe number is EVEN.")
+else:
+    print("\nThe number is ODD.")
+
+print("======================================")
+print("        Program Completed!")
+print("======================================")
